aa2b3c7fa959258ca7081b7c6bebf5b5150a1660
Malte Voss <22179782+vossmalte@users.noreply.github.com>
2023-03-09 16:07:12 +0100

    :lipstick: use footerheight to prevent overlap
    main content is now bottom-padded to leave space for footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,12 @@
-<script>
+<script lang="ts">
   import '../lib/css/tailwind.css';
   import '../lib/css/css-reset.css';
   import '../lib/css/global.css';
   import Footer from '../components/layout/Footer.svelte';
   import Header from '../components/layout/Header.svelte';
 
-  let headerheight = 0;
-  let footerheight = 0;
+  let headerheight: number;
+  let footerheight: number;
 </script>
 
 <svelte:head>
@@ -21,7 +21,10 @@
 <Header bind:headerheight />
 <div class="flex flex-row" style="min-height: calc(100vh - {headerheight}px - {footerheight}px);">
   <div class="w-0 bg-gradient-to-r from-interactive-200 to-background-50 sm:w-full" />
-  <main class="p-4 bg-background-50 max-w-5xl w-full sm:min-w-[600px]">
+  <main
+    class="p-4 bg-background-50 max-w-5xl w-full sm:min-w-[600px]"
+    style:padding-bottom="{footerheight + 8}px"
+  >
     <slot />
   </main>
   <div class="w-0 bg-gradient-to-r from-background-50 to-secondary-200 sm:w-full" />
