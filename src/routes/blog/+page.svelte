8c544cfcdda86ad3ea983f1a07c89330a2b10548
Malte Voss <22179782+vossmalte@users.noreply.github.com>
2023-03-11 23:50:30 +0100

    :sparkles: add a list of blog posts
    overview is good, introduction is missing

---
+++ src/routes/blog/+page.svelte
@@ -0,0 +1,20 @@
+<h1>Malte's Blog</h1>
+<svelte:head>
+  <title>Malte's Blog</title>
+</svelte:head>
+
+<ul>
+  <li><a href="blog/article/gitmoji-with-lazygit">Using gitmoji with lazygit</a></li>
+  <li>more to come...</li>
+</ul>
+
+<style>
+  ul {
+    gap: 1rem;
+    list-style-type: '\1F4DD';  
+  }
+  li {
+      margin-bottom: 1rem;
+      padding-left: 4px;
+    }
+</style>
