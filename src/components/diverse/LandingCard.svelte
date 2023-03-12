8e8e0cc2f60621db0e182686a5dda5ceb88ffda9
Malte Voss <22179782+vossmalte@users.noreply.github.com>
2023-03-12 02:06:53 +0100

    :zap: use css to hue rotate
    no js is best js

--- src/components/diverse/LandingCard.svelte
+++ src/components/diverse/LandingCard.svelte
@@ -1,56 +1,22 @@
 <script lang="ts">
-  import { onDestroy, onMount } from 'svelte';
-  import { tweened } from 'svelte/motion';
-
-  let running = false;
-
   export let backgroundUrl = 'https://images.unsplash.com/photo-1512095662389-83dfa94f3e0b';
   export let backgroundSize = '400%';
   export let backgroundPosition = '55% 70%';
   export let backgroundAnimation = 'frisbee';
-
-  // this is needed as background gradients cannot be animated with CSS
-  // maybe another way is: https://stackoverflow.com/questions/62135395/css-background-image-w-rotate-repeat-and-opacity
-  export const rotation = tweened(Math.random() * 360, {
-    duration: 4000
-  });
-
-  $: {
-    while (running) {
-      running = false;
-      rotation.set(0, { duration: 0 });
-      rotation.set(360).then(() => {
-        running = true;
-      });
-    }
-  }
-
-  onMount(() => {
-    running = true;
-  });
-
-  onDestroy(() => {
-    running = false;
-  });
 </script>
 
-<div
-  class="card"
-  style={`background: linear-gradient(${
-    $rotation + 217
-  }deg, rgba(255, 0, 0, 0.8), rgba(255, 0, 0, 0) 70.71%),			linear-gradient(${
-    $rotation + 127
-  }deg, rgba(0, 255, 0, 0.8), rgba(0, 255, 0, 0) 70.71%),	linear-gradient(${
-    $rotation + 336
-  }deg, rgba(0, 0, 255, 0.8), rgba(0, 0, 255, 0) 70.71%);`}
->
-  <div
-    class="inner-card {backgroundAnimation}"
-    style:background-image="url({backgroundUrl})"
-    style:background-size={backgroundSize}
-    style:background-position={backgroundPosition}
-  >
-    <div class="cta"><slot /></div>
+<div class="card">
+  <div class="hue-rotate">
+    <div
+      class="inner-card {backgroundAnimation}"
+      style:background-image="url({backgroundUrl})"
+      style:background-size={backgroundSize}
+      style:background-position={backgroundPosition}
+    >
+      <div class="blurry">
+        <div class="cta"><slot /></div>
+      </div>
+    </div>
   </div>
 </div>
 
@@ -58,7 +24,27 @@
   .card {
     width: 300px;
     height: 483px;
-    background: red;
+    clip-path: polygon(
+      10% 0%,
+      90% 0%,
+      100% 6.21%,
+      100% 93.8%,
+      90% 100%,
+      10% 100%,
+      0% 93.8%,
+      0% 6.21%
+    );
+    /* TODO: maybe a nicer gradient with hue values will result in nicer rotation */
+    background: linear-gradient(217deg, rgba(255, 0, 0, 0.8), rgba(255, 0, 0, 0) 70.71%),
+      linear-gradient(127deg, rgba(0, 255, 0, 0.8), rgba(0, 255, 0, 0) 70.71%),
+      linear-gradient(336deg, rgba(0, 0, 255, 0.8), rgba(0, 0, 255, 0) 70.71%);
+  }
+
+  .hue-rotate:hover {
+    padding: 16px;
+  }
+
+  .hue-rotate {
     clip-path: polygon(
       10% 0%,
       90% 0%,
@@ -70,11 +56,29 @@
       0% 6.21%
     );
     padding: 4px;
-    transition: 1s ease-in;
+    transition: padding 1s ease-in;
+    height: 100%;
+    animation: color-rotate 10s linear infinite;
   }
 
-  .card:hover {
-    padding: 16px;
+  .blurry {
+    height: 100%;
+    overflow: clip;
+    backdrop-filter: blur(10px);
+    clip-path: polygon(
+      10% 0%,
+      90% 0%,
+      100% 6.21%,
+      100% 93.8%,
+      90% 100%,
+      10% 100%,
+      0% 93.8%,
+      0% 6.21%
+    );
+    transition: backdrop-filter 1s ease-in;
+  }
+  .blurry:hover {
+    backdrop-filter: blur(0px);
   }
 
   .inner-card {
@@ -101,5 +105,14 @@
 
   .inner-card:hover .cta {
     scale: 1.3;
+  }
+
+  @keyframes color-rotate {
+    0% {
+      backdrop-filter: hue-rotate(0deg);
+    }
+    100% {
+      backdrop-filter: hue-rotate(359deg);
+    }
   }
 </style>
