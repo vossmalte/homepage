bf581ab253c26a929365768169d09f805d2e43af
Malte Voss <22179782+vossmalte@users.noreply.github.com>
2023-04-01 21:22:32 +0200

    🛂 (chore): add Haftungshinweis

--- src/components/diverse/Impressum.svelte
+++ src/components/diverse/Impressum.svelte
@@ -1,4 +1,7 @@
 <h1>Impressum</h1>
+
+<h2>Herausgeber</h2>
+
 <div>
   <p>M l e V s</p>
   <p>S p i n t . 1 7</p>
@@ -11,8 +14,16 @@
   <p>6 8 &nbsp; a l r h</p>
 </div>
 
+
+<h2>Haftungshinweis</h2>
+
+<p>Ich übernehme keine Haftung für die Inhalte externer Links. Für den Inhalt der verlinkten Seiten sind ausschließlich deren Betreiber verantwortlich.</p>
+
 <style>
   div + div {
     translate: 1ch -100%;
   }
+  h2 {
+      font-weight: 1000;
+    }
 </style>
