be2bad0f55b634e9389773e6593a056f44512131
Malte Voss <22179782+vossmalte@users.noreply.github.com>
2023-04-01 21:29:27 +0200

    ✨ (feat): fibonacci tiles

---
+++ src/components/fibonacci/FibSquare.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+  export let n = 10;
+
+  function fib(n: number): number {
+    if (n < 2) return n;
+    return fib(n - 1) + fib(n - 2);
+  }
+  let flexDirection: string;
+  let backgroundColor: string;
+  switch (n % 4) {
+    case 0: {
+      flexDirection = 'column';
+      backgroundColor = 'red';
+      break;
+    }
+    case 1: {
+      flexDirection = 'row';
+      backgroundColor = 'green';
+      break;
+    }
+    case 2: {
+      flexDirection = 'column-reverse';
+      backgroundColor = 'yellow';
+      break;
+    }
+    default: {
+      flexDirection = 'row-reverse';
+      backgroundColor = 'blue';
+      break;
+    }
+  }
+  const r = fib(n);
+  const R = r * 10;
+</script>
+
+<div style:flex-direction={flexDirection}>
+  <div
+    class="square"
+    style:height="{R}px"
+    style:width="{R}px"
+    style:font-size="{r / 3}rem"
+    style:background-color={backgroundColor}
+  >
+    <p>{fib(n)}</p>
+  </div>
+  {#if n > 1}
+    <svelte:self n={n - 1} />
+  {/if}
+</div>
+
+<style>
+  p {
+    margin: auto;
+  }
+  .square {
+    outline: 1px solid black;
+  }
+  div {
+    display: flex;
+    width: fit-content;
+  }
+</style>
---
+++ src/routes/fibonacci/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+  import FibSquare from '../../components/fibonacci/FibSquare.svelte';
+
+  let n = 8;
+</script>
+
+<h1>Fibonacci with TanStack Query</h1>
+<label><input type="range" step="1" bind:value={n} min="1" max="50" />{n}</label>
+<div style:transform="scale({20/Math.pow(1.5,n)})">
+  {#key n}
+    <FibSquare {n} />
+  {/key}
+</div>
+
+<style>
+  div {
+    transform-origin: 0 0;
+  }
+</style>
